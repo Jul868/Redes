3b67d0e5bae34a838c5d18adbe9a41bc2050a0fc
Jul868 <julian.alzate25@eia.edu.co>
2024-03-06 13:48:39 -0500

    ..

--- Lab_I2C/I2C_lab2.ino
+++ Lab_I2C/I2C_lab2.ino
@@ -1,2 +1,26 @@
-#include <wire.h>
+#include <Wire.h>
 
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop () {
+  // put your main code here, to run repeatedly:
+  Wire.beginTransmission(0x40);
+  Wire.write(0x00);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  delay(1000);
+  Wire.requestFrom(0x40, 2);
+  if (Wire.available()) {
+    int data = Wire.read();
+    Serial.println(data);
+  }
+  delay(1000);
+}
+ 
+ The code is not working. I am not able to read the data from the sensor. 
+ I have also tried to use the library 
