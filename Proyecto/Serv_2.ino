2d6512908068d8dee95676aa83790f8880eb168e
Jul868 <julian.alzate25@eia.edu.co>
2024-05-06 08:37:31 -0500

    adsadj

--- Proyecto/Serv_2.ino
+++ Proyecto/Serv_2.ino
@@ -1,66 +1,113 @@
+/*
+  Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+  Ported to Arduino ESP32 by Evandro Copercini
+  updated by chegewara and MoThunderz
+*/
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-BLEServer* pServer = NULL;
-BLEService* pService = NULL;
-BLECharacteristic *pLatitudeCharacteristic = NULL;
-BLECharacteristic *pLongitudeCharacteristic = NULL;
-BLECharacteristic *pAltitudeCharacteristic = NULL;
+// Initialize all pointers
+BLEServer* pServer = NULL;                        // Pointer to the server
+BLECharacteristic* pCharacteristic_1 = NULL;      // Pointer to Characteristic 1
+BLECharacteristic* pCharacteristic_2 = NULL;      // Pointer to Characteristic 2
 
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define LATITUDE_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-#define LONGITUDE_UUID "1c95d5e3-d8f7-413a-bf3d-7a2e5d7be87e"
-#define ALTITUDE_UUID "5ec11f33-dc1b-4e31-b9fb-2f9a274c9cdf"
+// Some variables to keep track on device connected
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
 
+// Variable that will continuously be increased and written to the client
+uint32_t value = 0;
+
+#define Latitude 37.7749
+#define Altitude 122.4194
+#define Longitude -122.4194
+
+
+#define SERVICE_UUID          "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID_1 "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define CHARACTERISTIC_UUID_2 "1c95d5e3-d8f7-413a-bf3d-7a2e5d7be87e"
+
+// Callback function that is called whenever a client is connected or disconnected
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
-      Serial.println("Client Connected");
-    }
+      deviceConnected = true;
+    };
 
     void onDisconnect(BLEServer* pServer) {
-      Serial.println("Client Disconnected");
+      deviceConnected = false;
     }
 };
 
 void setup() {
   Serial.begin(115200);
-  BLEDevice::init("ESP32 GPS Server");
+
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
-  pService = pServer->createService(SERVICE_UUID);
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  pLatitudeCharacteristic = pService->createCharacteristic(
-                               LATITUDE_UUID,
-                               BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
-                             );
-  pLatitudeCharacteristic->addDescriptor(new BLE2902());
-  pLatitudeCharacteristic->setValue("34.0522");
+  // Create a BLE Characteristic
+  pCharacteristic_1 = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_1,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );                   
 
-  pLongitudeCharacteristic = pService->createCharacteristic(
-                                LONGITUDE_UUID,
-                                BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
-                              );
-  pLongitudeCharacteristic->addDescriptor(new BLE2902());
-  pLongitudeCharacteristic->setValue("-118.2437");
+  pCharacteristic_2 = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_2,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |                      
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );  
 
-  pAltitudeCharacteristic = pService->createCharacteristic(
-                               ALTITUDE_UUID,
-                               BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
-                             );
-  pAltitudeCharacteristic->addDescriptor(new BLE2902());
-  pAltitudeCharacteristic->setValue("90m");
-
+  // Start the service
   pService->start();
+
+  // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->start();
-
-  Serial.println("Service Advertising Started");
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-  // This is left empty intentionally.
+    // notify changed value
+    if (deviceConnected) {
+      // pCharacteristic_1 is an integer that is increased with every second
+      // in the code below we send the value over to the client and increase the integer counter
+      pCharacteristic_1->setValue(value);
+      pCharacteristic_1->notify();
+      value++;
+
+
+      // Here the value is written to the Client using setValue();
+      String txValue = "String with random value from Server: " + String(Latitude);
+      pCharacteristic_2->setValue(txValue.c_str());
+      Serial.println("Characteristic 2 (setValue): " + txValue);
+
+      // In this example "delay" is used to delay with one second. This is of course a very basic 
+      // implementation to keep things simple. I recommend to use millis() for any production code
+      delay(1000);
+    }
+    // The code below keeps the connection status uptodate:
+    // Disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // Connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
 }
